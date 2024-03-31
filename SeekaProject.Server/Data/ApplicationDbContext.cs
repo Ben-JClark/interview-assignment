f6f49a8007db88774d63d4c3953c96f358067871
Ben-JClark <benclarkat@proton.me>
2024-04-01 11:53:10 +1300

    Initial upload
    Implemented required Product Model, DbContext, and required Product migration. Created ProductController.cs but no routes have been added

---
+++ SeekaProject.Server/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SeekaProject.Server.Models;
+
+namespace SeekaProject.Server.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext (DbContextOptions<ApplicationDbContext> options ): base( options )
+        {
+        }
+
+        public DbSet<SeekaProject.Server.Models.Product> Product { get; set; }
+    }
+}
